56530c12d35e44cd18c3dcd10e8bdea378ca55d7
Bob Larrick <larrick@gmail.com>
2011-01-11 22:40:06 -0500

    removing tinymce

--- lib/contentment/engine.rb
+++ lib/contentment/engine.rb
@@ -1,16 +1,19 @@
 require "contentment"
 require "rails"
 #require "action_controller"
+
+# http://edgeapi.rubyonrails.org/classes/Rails/Engine.html
 module Contentment
   class Engine < Rails::Engine
-		engine_name :contentment
+		# are these not used? engine_name, railtie_name
+		# engine_name "contentment"
 
     # # We can add all of the public assets from our engine and make them
     # # available to use.  This allows us to use javascripts, images, stylesheets
     # # etc.
-    # initializer "static assets" do |app|
-    #   app.middleware.use ::ActionDispatch::Static, "#{root}/public"
-    # end
+    initializer "static assets" do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
  
   end
 end
