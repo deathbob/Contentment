3fe9186edc81f01047f0e1ab0c9529014ec4e6d3
Bob Larrick <larrick@gmail.com>
2011-01-14 00:18:18 -0500

    Adding documentation and tests

--- lib/generators/contentment/contentment_views_templates/contents_helper.rb
+++ lib/generators/contentment/contentment_views_templates/contents_helper.rb
@@ -1,4 +1,7 @@
 module ContentsHelper
+
+	# Is a helper really necessary?  Apparently I thought so at some point.  <%= f.select :tipe, content_tipe_options %>
+	# When the helper is longer than what it replaces you've got a problem.  
 	def content_tipe_options
     Content.tipes
   end
--- lib/generators/contentment/templates/content.rb
+++ lib/generators/contentment/templates/content.rb
@@ -1,10 +1,12 @@
 class Content < ActiveRecord::Base
 
-# ADJUST_POSTIONS DOES NOT WORK RIGHT NOW
-#	before_save :adjust_positions
+	before_save :adjust_positions
+	
 	
 	class << self; attr_accessor :tipes; end
   
+	# Put all the different kinds of content you will have in here to get a select helper and some named scopes 
+	# generated for you. 
   @tipes = [
 	# "Billboard",
 	# "What You Learn",
@@ -16,23 +18,27 @@ class Content < ActiveRecord::Base
 	# "Navigation",
 	]
   
+	# Pretty much always want to only find content of one tipe at a time, so generate scopes for all tipes.
   @tipes.each do |thing|
     scope "for_#{thing.downcase.gsub(' ', '_')}".intern, where("tipe = ?", thing)
   end
 
+	# It's pretty common to want the ability to hide some content.  Use the visible scope liberally.
 	scope :visible, lambda {|x| where(:visible => x)}
 
-
+	# Returns all Content with like tipe.
 	def brothers
 		Content.where("tipe = ?", self.tipe).where("id NOT in (#{self.id})")
 	end
 
 	private
+		#  It's a pain with ordered content to have to manually update each piece of content when rearranging them.  
+		#  adjust_positions will make sure when you change the position of one content, all other content of that tipe
+		#  has position changed to keep a reasonable order.  
   	def adjust_positions
 			return true unless changed.include?('position')	
 			return true unless brothers.map{|x| x.position}.include?(self.position)
 			
-			brothers.where("position < ?", self.position).update_all("position = (position - 1)")
 			brothers.where("position >= ?", self.position).update_all("position = (position + 1)")
 		end
 		
