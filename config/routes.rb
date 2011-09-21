3a13d407ca006ed9e07d8e2594e9287d8edfd95b
Bob Larrick <larrick@gmail.com>
2011-09-21 10:20:19 -0400

    Removing route declaration in favor of the host app doing that

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # We are adding full resources for bird and declaring which controller to use
-  resources :contents#, :controller => 'baby_dove/birds'
+#  resources :contents
 
   # If you had nests and wanted to nest your nests you would do:
 
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   # the routes from the engine listed after any routes you define in the main
   # app.
 
-#  root :to => "baby_dove/birds#index"
-  
+
+
 end
